8202a5086eaecfaaf8cd8004670189f52cf6d338
IkechukwuAbuah <kelvin.abuah@gmail.com>
2022-09-21 01:17:18 +0100

    5. Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,14 +9,43 @@ contract Token {
     uint256 public decimals = 18; //Number of decimals in our contract
     uint256 public totalSupply;
 
-    //Track Balances
     mapping (address => uint256) public balanceOf;
-    // Send Tokens
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+event Transfer( 
+    address indexed from,
+    address indexed to, 
+    uint256 value
+    );
+
+
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint256 _totalSupply
+        ){
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply *(10**decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) 
+        public 
+        returns (bool success)
+    {
+
+        //Require that sender has enough tokens to spend
+        require(balanceOf[msg.sender]>=_value); 
+        require(_to !=address (0));
+
+        //Deduct tokens from spender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        //Credit tokens to receiver
+        balanceOf [_to] = balanceOf[_to] + _value;
+        
+        //Emit Event
+        emit Transfer (msg.sender, _to, _value);
+         
+         return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,22 +1,21 @@
-const {expect} = require('chai'); //pull expect function from chai library
-const {ethers} = require ('hardhat'); //pull ethers library from hardhat library
+const { expect } = require('chai'); //pull expect function from chai library
+const { ethers } = require ('hardhat'); //pull ethers library from hardhat library
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 describe ('Token', () => {//All tests go inside here..
-    let token, accounts, deployer
+    let token, accounts, deployer, receiver
     
-
     beforeEach(async () => {
 
         const Token = await ethers.getContractFactory('Token')
-        token = await Token.deploy('Dapp University', 'DAPP','1000000000000000000000000')
+        token = await Token.deploy('Dapp University', 'DAPP','1000000')
 
         accounts = await ethers.getSigners() //gets all accounts - will return array
         deployer = accounts[0] //will get the first value in the array [0]
+        receiver = accounts[1] 
     })
-
 
     describe ('Deployment', () => { //Test for deployment
         const name = 'Dapp University'
@@ -25,35 +24,81 @@ describe ('Token', () => {//All tests go inside here..
         const totalSupply = tokens('1000000')
 
 
-        it('has correct name', async () => { //Name
+        it('- has correct name', async () => { //Name
             //Read token name & check for correctnes
             expect(await token.name()).to.equal(name)
         })
     
     
-        it('has correct symbol', async () => { //Symbol
+        it('- has correct symbol', async () => { //Symbol
             //Read token sybol & check for correctness
             expect(await token.symbol()).to.equal(symbol)
         })
     
     
-        it('has correct decimals', async () => { //decimals
+        it('- has correct decimals', async () => { //decimals
             //Read token symbol & check for correctness
             expect(await token.decimals()).to.equal(decimals)
         })
     
     
-        it('has correct total Supply', async () => {  //Read token total supply & check for correctness
-            expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+        it('- has correct total Supply', async () => {  //Read token total supply & check for correctness
+            expect(await token.totalSupply()).to.equal(totalSupply)
         })
 
 
-        it('assigns total supply to deployer', async () => {  //Read token total supply & check for correctness
-            expect(await token.balanceOf(deployer.address)).to.equal(tokens(totalSupply))
+        it('- assigns total supply to deployer', async () => {  //Read token total supply & check for correctness
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
         })
     })
 
-
     //Describe Spending
+    describe('Sending Tokens', () => {
+        let amount, transaction, result//amount to be tranfered between deployer and receiver. Derived from "token"
+        
+        describe('Success', ()=> {
+
+            beforeEach(async() => {
+                amount = tokens(100)
+                //Transfer tokens
+                transaction = await token.connect(deployer).transfer(receiver.address, amount) // connects deployer wallet to smart contract
+                result = await transaction.wait() //waits for the entire transaction to finish on the block before moving on to the next step
+            })
+    
+            it('- transfers token balances', async () => {
+                //Ensure tokens were transfered & balance changes
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+              
+            })
+    
+            it('- emits a Transfer event', async () => {
+                //check for events
+            const event = result.events[0] 
+            expect (event.event).to.equal('Transfer')
+            
+            //check for arguments
+            const args = event.args 
+            expect(args.from).to.equal(deployer.address)
+            expect(args.to).to.equal(receiver.address)
+            expect(args.to).to.equal(receiver.address)
+            expect(args.value).to.equal(amount)
+            })
+        })
+
+        describe ('Failure', () =>{
+            it('- rejects insufficent balances', async () => {
+                //To test, transfer more tokens than the deployer has e.g 100M. For sufficent balance txns, this test will fail e.g 10 tokens
+                const invalidAmount = tokens(100000000)
+                await expect (token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+            })
+
+            //Reject invalid receipients
+            it('- rejects invalid receipts', async () =>{
+                const amount = tokens(100)
+                await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000',amount)).to.be.reverted
+            })
+        })
+    })
     //Describe approving
 })
