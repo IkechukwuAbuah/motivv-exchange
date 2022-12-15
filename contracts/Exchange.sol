84ebb29281b53d4c7c072fa162888c7acd389d38
IkechukwuAbuah <kelvin.abuah@gmail.com>
2022-12-15 01:35:20 +0100

    13. Fill Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,19 +11,23 @@ contract Exchange {
     mapping (uint256 => _Order)public orders;
     uint256 public orderCount;
     mapping(uint256 => bool) public orderCancelled; // boolean -  true or false 
+    mapping(uint256 => bool) public orderFilled; // boolean -  true or false whether the order has been filled
+
     //Orders mapping 
 
     event Deposit(
         address token, 
         address user, 
         uint256 amount, 
-        uint256 balance);
+        uint256 balance
+    );
 
     event Withdraw(
         address token, 
         address user, 
         uint256 amount,
-        uint256 balance);
+        uint256 balance
+    );
 
     event Order(
         uint256 id,        
@@ -32,7 +36,8 @@ contract Exchange {
         uint amountGet, 
         address tokenGive,  
         uint amountGive, 
-        uint256 timestamp);
+        uint256 timestamp
+    );
 
     event Cancel(
         uint256 id,        
@@ -41,8 +46,21 @@ contract Exchange {
         uint amountGet, 
         address tokenGive,  
         uint amountGive, 
-        uint256 timestamp);
+        uint256 timestamp
+    );
 
+    event Trade(
+        uint256 id,
+        address user,   
+        address tokenGet,
+        uint amountGet, 
+        address tokenGive,  
+        uint amountGive, 
+        address creator,
+        uint256 timestamp
+    );
+
+    
     //A way to model the order
     struct _Order{
         //Attributes of an order
@@ -117,7 +135,7 @@ contract Exchange {
 
 
             //Instatiate ORDER
-            orderCount = orderCount + 1;
+            orderCount++;
 
             //Token Give (the token they want to sepend) - which token and how much?
             //Token Get (the token they want to receive) - which token and how much?
@@ -167,6 +185,77 @@ contract Exchange {
                 _order.tokenGive,
                 _order.amountGive,
                 block.timestamp
-            ); 
+            );
         }
+    //-----------------------------------------
+    //EXECUTING ORDERS
+    function fillOrder(uint256 _id) public {
+
+    //1. Mustbe valid orderID
+    require(_id>0 && _id <=orderCount, "Order does not exist");
+    //2. Order can't be filled
+    require(!orderFilled[_id]);
+    //3. Order can't be cancelled
+    require(!orderCancelled[_id]);
+    //Fetch Order from Storage
+    _Order storage _order = orders[_id];
+
+    //Execute trades
+    _trade(
+        _order.id, 
+        _order.user,
+        _order.tokenGet,
+        _order.amountGet,
+        _order.tokenGive,
+        _order.amountGive
+        );
+
+    //Mark Order as Filled
+    orderFilled[_order.id] = true;
+
+
+    }
+
+    //Trading/Swapping Tokens
+    function _trade(
+        uint256 _orderId, 
+        address _user, //Order's user i.e the person filling the order
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+        ) internal{
+
+            //Fee is paid by the user who filled the order (msg.sender)
+            //Fee is deducted from _amountGet
+            uint256 _feeAmount = (_amountGet * feePercent) /100;
+
+            //Trades happen here
+            //msg.sender filled the order, _user created the order
+            tokens[_tokenGet][msg.sender]=tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount); //reduce tokenget from msg.sender's tokens
+            tokens[_tokenGet][_user]= tokens[_tokenGet][_user]+_amountGet; //add tokenget deducted from msg.sender to _user's tokens
+
+            //Charge Fees
+            tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount] + _feeAmount;
+
+            //Token Giving
+
+            tokens[_tokenGive][_user]= tokens[_tokenGive][_user] - _amountGive;
+            tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender] + _amountGive;
+
+            //Emit trade Event
+            emit Trade(
+                _orderId,
+                msg.sender, //user
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                _user, //creator
+                block.timestamp
+            );
+    }
+
 }   
+
+
