eeffbd86e9ffced3227e02783c364ae02a51f3f9
IkechukwuAbuah <kelvin.abuah@gmail.com>
2023-02-02 00:59:07 +0100

    mumbai deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 const privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -10,7 +13,13 @@ module.exports = {
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(','),
+    },
+    hardhat: {
     }
-
-  }
+    ,
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
+  },
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,10 +41,9 @@ const Navbar = () => {
           <select name="networks" id="networks" value={config[chainId] ? `0x${chainId.toString(16)}` : `0`} onChange={networkHandler}>
             <option value="0" disabled>Select Network</option>
             <option value="0x7A69">Localhost</option>
-            <option value="0x2 a">Kovan</option>
+            <option value="0x2a">Kovan</option>
             <option value="0x5">Goerli</option>
-
-            
+            <option value="0x13881">Mumbai</option>
           </select>
         )}
 
